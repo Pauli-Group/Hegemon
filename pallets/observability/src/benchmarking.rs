f59368e834e013c438326dee9513bf7efa8ce29c
Pierre-Luc Dallaire-Demers <pldallairedemers@users.noreply.github.com>
2025-11-21 19:57:47 -0700

    Add observability pallet with quotas and metrics events

---
+++ pallets/observability/src/benchmarking.rs
@@ -0,0 +1,41 @@
+#![cfg(feature = "runtime-benchmarks")]
+
+use super::*;
+use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_system::RawOrigin;
+
+benchmarks! {
+    set_quota {
+        let actor: T::AccountId = whitelisted_caller();
+        let max_usage: u128 = 1_000;
+        let rate_hint: u64 = 10;
+        let origin: OriginFor<T> = RawOrigin::Root.into();
+    }: _(origin, actor.clone(), max_usage, rate_hint)
+    verify {
+        assert!(Quotas::<T>::contains_key(&actor));
+    }
+
+    record_self_usage {
+        let actor: T::AccountId = whitelisted_caller();
+        let amount: u64 = 5;
+    }: _(RawOrigin::Signed(actor.clone()), amount)
+    verify {
+        let usage = UsageCounters::<T>::get(&actor).expect("usage stored");
+        assert!(usage.total_usage >= amount as u128);
+    }
+
+    emit_snapshot {
+        let actor: T::AccountId = whitelisted_caller();
+        Quotas::<T>::insert(&actor, Quota { max_usage: 50, rate_limit_per_block: 5 });
+        UsageCounters::<T>::insert(&actor, UsageCounter::new(20, 20, Default::default()));
+    }: _(RawOrigin::Signed(actor.clone()))
+    verify {
+        assert!(UsageCounters::<T>::contains_key(&actor));
+    }
+}
+
+impl_benchmark_test_suite!(
+    Pallet,
+    crate::tests::new_test_ext(),
+    crate::tests::TestRuntime
+);
