4d78726e39a54a073ab772440396b49690b483fd
Pierre-Luc Dallaire-Demers <pldallairedemers@users.noreply.github.com>
2025-11-21 19:15:26 -0700

    Add settlement pallet with batching and disputes

---
+++ pallets/settlement/src/lib.rs
@@ -0,0 +1,557 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+use codec::{Decode, DecodeWithMemTracking, Encode};
+use frame_support::pallet_prelude::*;
+use frame_support::traits::{EnsureOrigin, StorageVersion};
+use frame_system::offchain::{AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer};
+use frame_system::pallet_prelude::*;
+use scale_info::TypeInfo;
+use sp_runtime::traits::{AtLeast32BitUnsigned, Hash};
+use sp_runtime::KeyTypeId;
+use sp_runtime::RuntimeDebug;
+use sp_std::vec::Vec;
+
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"sett");
+
+pub mod crypto {
+    use super::KEY_TYPE;
+    use sp_runtime::app_crypto::{app_crypto, sr25519};
+
+    app_crypto!(sr25519, KEY_TYPE);
+}
+
+pub mod weights;
+
+#[derive(
+    Encode, Decode, DecodeWithMemTracking, Clone, Copy, Eq, PartialEq, RuntimeDebug, TypeInfo,
+)]
+pub enum NettingKind {
+    Bilateral,
+    Multilateral,
+}
+
+#[derive(Encode, Decode, DecodeWithMemTracking, Clone, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct Leg<AccountId, AssetId, Balance> {
+    pub from: AccountId,
+    pub to: AccountId,
+    pub asset: AssetId,
+    pub amount: Balance,
+}
+
+#[derive(Encode, Decode, DecodeWithMemTracking, Clone, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct Instruction<AccountId, AssetId, Balance, BlockNumber> {
+    pub id: u64,
+    pub legs: Vec<Leg<AccountId, AssetId, Balance>>,
+    pub netting: NettingKind,
+    pub memo: Vec<u8>,
+    pub submitted_at: BlockNumber,
+}
+
+#[derive(Encode, Decode, DecodeWithMemTracking, Clone, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct BatchCommitment<Hash, AccountId> {
+    pub id: u64,
+    pub instructions: Vec<u64>,
+    pub commitment: Hash,
+    pub nullifiers: Vec<Hash>,
+    pub proof: Vec<u8>,
+    pub submitted_by: AccountId,
+    pub disputed: bool,
+}
+
+#[derive(Encode, Decode, DecodeWithMemTracking, Clone, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct StateChannelCommitment<AccountId, Hash, BlockNumber> {
+    pub channel_id: Hash,
+    pub participants: Vec<AccountId>,
+    pub version: u32,
+    pub balance_root: Hash,
+    pub closing_height: BlockNumber,
+    pub signature: Vec<u8>,
+    pub disputed: bool,
+    pub escalated: bool,
+}
+
+pub type InstructionRecord<T> = Instruction<
+    <T as frame_system::Config>::AccountId,
+    <T as Config>::AssetId,
+    <T as Config>::Balance,
+    BlockNumberFor<T>,
+>;
+
+pub type BatchRecord<T> =
+    BatchCommitment<<T as frame_system::Config>::Hash, <T as frame_system::Config>::AccountId>;
+
+pub type StateChannelRecord<T> = StateChannelCommitment<
+    <T as frame_system::Config>::AccountId,
+    <T as frame_system::Config>::Hash,
+    BlockNumberFor<T>,
+>;
+
+pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
+pub trait ProofVerifier<Hash> {
+    fn verify(commitment: &Hash, proof: &[u8], verification_key: &[u8]) -> bool;
+}
+
+pub struct AcceptAllProofs;
+
+impl<Hash> ProofVerifier<Hash> for AcceptAllProofs {
+    fn verify(_commitment: &Hash, _proof: &[u8], _verification_key: &[u8]) -> bool {
+        true
+    }
+}
+
+#[frame_support::pallet]
+pub mod pallet {
+    use super::*;
+    use crate::weights::WeightInfo;
+
+    #[pallet::pallet]
+    #[pallet::without_storage_info]
+    #[pallet::storage_version(STORAGE_VERSION)]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config + CreateSignedTransaction<Call<Self>> {
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        type AssetId: Parameter + Member + Copy + MaxEncodedLen;
+        type Balance: Parameter + Member + AtLeast32BitUnsigned + Default + MaxEncodedLen + Copy;
+        type VerificationKeyId: Parameter + Member + MaxEncodedLen + Copy + Default;
+        type GovernanceOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+        type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+        type ProofVerifier: ProofVerifier<Self::Hash>;
+        type WeightInfo: WeightInfo;
+        #[pallet::constant]
+        type MaxLegs: Get<u32>;
+        #[pallet::constant]
+        type MaxMemo: Get<u32>;
+        #[pallet::constant]
+        type MaxPendingInstructions: Get<u32>;
+        #[pallet::constant]
+        type MaxParticipants: Get<u32>;
+        #[pallet::constant]
+        type MaxNullifiers: Get<u32>;
+        #[pallet::constant]
+        type MaxProofSize: Get<u32>;
+        #[pallet::constant]
+        type MaxVerificationKeySize: Get<u32>;
+        #[pallet::constant]
+        type DefaultVerificationKey: Get<Self::VerificationKeyId>;
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn next_instruction_id)]
+    pub type NextInstructionId<T> = StorageValue<_, u64, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn next_batch_id)]
+    pub type NextBatchId<T> = StorageValue<_, u64, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn pending_queue)]
+    pub type PendingQueue<T: Config> =
+        StorageValue<_, BoundedVec<u64, <T as Config>::MaxPendingInstructions>, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn instructions)]
+    pub type Instructions<T: Config> = StorageMap<_, Blake2_128Concat, u64, InstructionRecord<T>>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn batch_commitments)]
+    pub type BatchCommitments<T: Config> = StorageMap<_, Blake2_128Concat, u64, BatchRecord<T>>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn verification_keys)]
+    pub type VerificationKeys<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat,
+        T::VerificationKeyId,
+        BoundedVec<u8, <T as Config>::MaxVerificationKeySize>,
+    >;
+
+    #[pallet::storage]
+    #[pallet::getter(fn nullifier_used)]
+    pub type Nullifiers<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, bool, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn state_channels)]
+    pub type StateChannels<T: Config> =
+        StorageMap<_, Blake2_128Concat, T::Hash, StateChannelRecord<T>>;
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        InstructionQueued {
+            id: u64,
+            who: T::AccountId,
+            netting: NettingKind,
+        },
+        BatchSubmitted {
+            id: u64,
+            who: T::AccountId,
+        },
+        VerificationKeyRegistered {
+            id: T::VerificationKeyId,
+        },
+        NullifierConsumed {
+            nullifier: T::Hash,
+        },
+        StateChannelCommitted {
+            channel: T::Hash,
+            version: u32,
+        },
+        StateChannelDisputed {
+            channel: T::Hash,
+        },
+        DisputeEscalated {
+            channel: T::Hash,
+        },
+        DisputeResolved {
+            batch: Option<u64>,
+            channel: Option<T::Hash>,
+        },
+        BatchRolledBack {
+            id: u64,
+        },
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        QueueFull,
+        InstructionMissing,
+        BatchExists,
+        BatchMissing,
+        VerificationKeyMissing,
+        ProofInvalid,
+        NullifierReused,
+        UnknownChannel,
+        DisputeInactive,
+        DisputeActive,
+        Escalated,
+    }
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn offchain_worker(n: BlockNumberFor<T>) {
+            let queue = PendingQueue::<T>::get();
+            if queue.is_empty() {
+                return;
+            }
+
+            let commitment_hash = T::Hashing::hash_of(&queue);
+            let nullifier = T::Hashing::hash_of(&(commitment_hash, n));
+            let proof: BoundedVec<u8, T::MaxProofSize> = BoundedVec::truncate_from(Vec::new());
+            let nullifiers: BoundedVec<T::Hash, T::MaxNullifiers> =
+                BoundedVec::truncate_from(vec![nullifier]);
+
+            let signer = Signer::<T, T::AuthorityId>::any_account();
+            let _ = signer.send_signed_transaction(|_account| Call::submit_batch {
+                instructions: queue.clone(),
+                commitment: commitment_hash,
+                proof: proof.clone(),
+                nullifiers: nullifiers.clone(),
+                key: T::DefaultVerificationKey::get(),
+            });
+        }
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(T::WeightInfo::submit_instruction())]
+        pub fn submit_instruction(
+            origin: OriginFor<T>,
+            legs: BoundedVec<Leg<T::AccountId, T::AssetId, T::Balance>, T::MaxLegs>,
+            netting: NettingKind,
+            memo: BoundedVec<u8, T::MaxMemo>,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            PendingQueue::<T>::try_mutate(|queue| -> Result<(), Error<T>> {
+                if queue.len() as u32 >= T::MaxPendingInstructions::get() {
+                    return Err(Error::<T>::QueueFull);
+                }
+                let id = NextInstructionId::<T>::get();
+                NextInstructionId::<T>::put(id.saturating_add(1));
+
+                let record = InstructionRecord::<T> {
+                    id,
+                    legs: legs.into_inner(),
+                    netting,
+                    memo: memo.into_inner(),
+                    submitted_at: <frame_system::Pallet<T>>::block_number(),
+                };
+                Instructions::<T>::insert(id, record);
+                queue.try_push(id).map_err(|_| Error::<T>::QueueFull)?;
+                Ok(())
+            })?;
+
+            Self::deposit_event(Event::InstructionQueued {
+                id: NextInstructionId::<T>::get().saturating_sub(1),
+                who,
+                netting,
+            });
+            Ok(())
+        }
+
+        #[pallet::weight(T::WeightInfo::submit_batch())]
+        pub fn submit_batch(
+            origin: OriginFor<T>,
+            instructions: BoundedVec<u64, T::MaxPendingInstructions>,
+            commitment: T::Hash,
+            proof: BoundedVec<u8, T::MaxProofSize>,
+            nullifiers: BoundedVec<T::Hash, T::MaxNullifiers>,
+            key: T::VerificationKeyId,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            let verification_key =
+                VerificationKeys::<T>::get(&key).ok_or(Error::<T>::VerificationKeyMissing)?;
+
+            ensure!(
+                T::ProofVerifier::verify(&commitment, &proof, &verification_key),
+                Error::<T>::ProofInvalid
+            );
+
+            ensure!(
+                nullifiers.len() as u32 <= T::MaxNullifiers::get(),
+                Error::<T>::NullifierReused
+            );
+            for n in nullifiers.iter() {
+                ensure!(!Nullifiers::<T>::get(n), Error::<T>::NullifierReused);
+            }
+
+            for instr in instructions.iter() {
+                ensure!(
+                    Instructions::<T>::contains_key(instr),
+                    Error::<T>::InstructionMissing
+                );
+            }
+
+            let id = NextBatchId::<T>::get();
+            ensure!(
+                !BatchCommitments::<T>::contains_key(id),
+                Error::<T>::BatchExists
+            );
+
+            for n in nullifiers.iter() {
+                Nullifiers::<T>::insert(n, true);
+                Self::deposit_event(Event::NullifierConsumed { nullifier: *n });
+            }
+
+            NextBatchId::<T>::put(id.saturating_add(1));
+            let stored = BatchRecord::<T> {
+                id,
+                instructions: instructions.clone().into_inner(),
+                commitment,
+                nullifiers: nullifiers.into_inner(),
+                proof: proof.into_inner(),
+                submitted_by: who.clone(),
+                disputed: false,
+            };
+            BatchCommitments::<T>::insert(id, stored);
+
+            PendingQueue::<T>::mutate(|queue| {
+                queue.retain(|instr_id| !instructions.contains(instr_id));
+            });
+
+            Self::deposit_event(Event::BatchSubmitted { id, who });
+            Ok(())
+        }
+
+        #[pallet::weight(T::WeightInfo::register_key())]
+        pub fn register_key(
+            origin: OriginFor<T>,
+            key: T::VerificationKeyId,
+            bytes: BoundedVec<u8, T::MaxVerificationKeySize>,
+        ) -> DispatchResult {
+            let _ = ensure_signed(origin)?;
+            VerificationKeys::<T>::insert(&key, bytes);
+            Self::deposit_event(Event::VerificationKeyRegistered { id: key });
+            Ok(())
+        }
+
+        #[pallet::weight(T::WeightInfo::commit_state_channel())]
+        pub fn commit_state_channel(
+            origin: OriginFor<T>,
+            channel_id: T::Hash,
+            participants: BoundedVec<T::AccountId, T::MaxParticipants>,
+            version: u32,
+            balance_root: T::Hash,
+            closing_height: BlockNumberFor<T>,
+            signature: BoundedVec<u8, T::MaxProofSize>,
+        ) -> DispatchResult {
+            let _ = ensure_signed(origin)?;
+            let record = StateChannelRecord::<T> {
+                channel_id,
+                participants: participants.into_inner(),
+                version,
+                balance_root,
+                closing_height,
+                signature: signature.into_inner(),
+                disputed: false,
+                escalated: false,
+            };
+            StateChannels::<T>::insert(channel_id, record);
+            Self::deposit_event(Event::StateChannelCommitted {
+                channel: channel_id,
+                version,
+            });
+            Ok(())
+        }
+
+        #[pallet::weight(T::WeightInfo::dispute_state_channel())]
+        pub fn dispute_state_channel(origin: OriginFor<T>, channel_id: T::Hash) -> DispatchResult {
+            let _ = ensure_signed(origin)?;
+            StateChannels::<T>::try_mutate(channel_id, |maybe_channel| -> Result<(), Error<T>> {
+                let channel = maybe_channel.as_mut().ok_or(Error::<T>::UnknownChannel)?;
+                ensure!(!channel.disputed, Error::<T>::DisputeActive);
+                channel.disputed = true;
+                Ok(())
+            })?;
+            Self::deposit_event(Event::StateChannelDisputed {
+                channel: channel_id,
+            });
+            Ok(())
+        }
+
+        #[pallet::weight(T::WeightInfo::escalate_dispute())]
+        pub fn escalate_dispute(origin: OriginFor<T>, channel_id: T::Hash) -> DispatchResult {
+            T::GovernanceOrigin::ensure_origin(origin)?;
+            StateChannels::<T>::try_mutate(channel_id, |maybe_channel| -> Result<(), Error<T>> {
+                let channel = maybe_channel.as_mut().ok_or(Error::<T>::UnknownChannel)?;
+                ensure!(channel.disputed, Error::<T>::DisputeInactive);
+                ensure!(!channel.escalated, Error::<T>::Escalated);
+                channel.escalated = true;
+                Ok(())
+            })?;
+            Self::deposit_event(Event::DisputeEscalated {
+                channel: channel_id,
+            });
+            Ok(())
+        }
+
+        #[pallet::weight(T::WeightInfo::resolve_dispute())]
+        pub fn resolve_dispute(
+            origin: OriginFor<T>,
+            batch_id: Option<u64>,
+            channel_id: Option<T::Hash>,
+            rollback: bool,
+        ) -> DispatchResult {
+            T::GovernanceOrigin::ensure_origin(origin)?;
+
+            if let Some(id) = batch_id {
+                BatchCommitments::<T>::try_mutate(id, |maybe_batch| -> Result<(), Error<T>> {
+                    let batch = maybe_batch.as_mut().ok_or(Error::<T>::BatchMissing)?;
+                    batch.disputed = false;
+                    if rollback {
+                        Self::rollback_batch_internal(id, batch)?;
+                        *maybe_batch = None;
+                    }
+                    Ok(())
+                })?;
+            }
+
+            if let Some(channel) = channel_id {
+                StateChannels::<T>::try_mutate(channel, |maybe_channel| -> Result<(), Error<T>> {
+                    let channel_state = maybe_channel.as_mut().ok_or(Error::<T>::UnknownChannel)?;
+                    ensure!(channel_state.disputed, Error::<T>::DisputeInactive);
+                    channel_state.disputed = false;
+                    channel_state.escalated = false;
+                    Ok(())
+                })?;
+            }
+
+            Self::deposit_event(Event::DisputeResolved {
+                batch: batch_id,
+                channel: channel_id,
+            });
+            Ok(())
+        }
+
+        #[pallet::weight(T::WeightInfo::rollback_batch())]
+        pub fn rollback_batch(origin: OriginFor<T>, id: u64) -> DispatchResult {
+            T::GovernanceOrigin::ensure_origin(origin)?;
+            BatchCommitments::<T>::try_mutate_exists(id, |maybe_batch| -> Result<(), Error<T>> {
+                let batch = maybe_batch.as_mut().ok_or(Error::<T>::BatchMissing)?;
+                Self::rollback_batch_internal(id, batch)?;
+                *maybe_batch = None;
+                Ok(())
+            })?;
+            Self::deposit_event(Event::BatchRolledBack { id });
+            Ok(())
+        }
+    }
+
+    impl<T: Config> Pallet<T> {
+        fn rollback_batch_internal(id: u64, batch: &BatchRecord<T>) -> Result<(), Error<T>> {
+            PendingQueue::<T>::mutate(|queue| {
+                for instr in batch.instructions.iter() {
+                    let _ = queue.try_push(*instr);
+                }
+            });
+
+            for n in batch.nullifiers.iter() {
+                Nullifiers::<T>::remove(n);
+            }
+
+            let _ = id; // placeholder for potential auditing hooks
+            Ok(())
+        }
+    }
+}
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking {
+    use super::*;
+    use crate::weights::WeightInfo;
+    use frame_benchmarking::v2::*;
+    use frame_system::RawOrigin;
+
+    #[benchmarks]
+    mod benches {
+        use super::*;
+
+        #[benchmark]
+        fn submit_instruction() -> Result<(), BenchmarkError> {
+            let caller: T::AccountId = whitelisted_caller();
+            let leg = Leg::<T::AccountId, T::AssetId, T::Balance> {
+                from: caller.clone(),
+                to: caller.clone(),
+                asset: Default::default(),
+                amount: Default::default(),
+            };
+            let legs = BoundedVec::truncate_from(vec![leg]);
+            let memo = BoundedVec::truncate_from(vec![0u8; T::MaxMemo::get() as usize]);
+            #[extrinsic_call]
+            submit_instruction(
+                RawOrigin::Signed(caller),
+                legs,
+                NettingKind::Bilateral,
+                memo,
+            );
+            Ok(())
+        }
+
+        #[benchmark]
+        fn submit_batch() -> Result<(), BenchmarkError> {
+            let caller: T::AccountId = whitelisted_caller();
+            let instructions: BoundedVec<u64, T::MaxPendingInstructions> =
+                BoundedVec::truncate_from(vec![]);
+            let proof = BoundedVec::truncate_from(vec![]);
+            let nullifiers = BoundedVec::truncate_from(vec![]);
+            #[extrinsic_call]
+            submit_batch(
+                RawOrigin::Signed(caller),
+                instructions,
+                Default::default(),
+                proof,
+                nullifiers,
+                T::DefaultVerificationKey::get(),
+            );
+            Ok(())
+        }
+
+        impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
+    }
+}
