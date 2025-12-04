289baea31eadf2fe3011915d5456a8f72608a657
willpiam <willdoyle422@gmail.com>
2025-12-04 17:58:37 -0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 
-HEGEMON_MINE=1 
-HEGEMON_MINE_THREADS=4 
-# cargo run --bin hegemon-node --features substrate -- --dev --name NodeB --base-path /tmp/hegemon-node-b --bootnodes "/ip4/75.155.93.185/tcp/30333/p2p/fdb0bc2ae7342e599c699d04b62baa5815a279f4b920fdb3ce9ba958fb752e7c"
-
-./target/release/hegemon-node --dev --base-path /tmp/node2 --port 30334 --rpc-port 9945 --bootnodes "/ip4/75.155.93.185/tcp/30333/p2p/fdb0bc2ae7342e599c699d04b62baa5815a279f4b920fdb3ce9ba958fb752e7c"
+( HEGEMON_MINE=1   HEGEMON_SEEDS="75.155.93.185:30333"   HEGEMON_MINER_ADDRESS=$(./target/release/wallet status --store ~/.hegemon-wallet --passphrase "password" 2>/dev/null | grep "Shielded Address:" | awk '{print $3}')   ./target/release/hegemon-node     --base-path ~/.hegemon-node     --chain config/dev-chainspec.json     --rpc-port 9944     --rpc-cors all     --name "WilliamNode"; ) > out.txt 2>&1
