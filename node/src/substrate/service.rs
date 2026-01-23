6f5ff2884714f781fe486737c0b5ca4c704146db
pldallairedemers <dallairedemers@gmail.com>
2026-01-23 14:07:09 -0700

    feat: add CashVM bridge STARK covenant ExecPlan and link from top-level plan

--- node/src/substrate/service.rs
+++ node/src/substrate/service.rs
@@ -1361,6 +1361,9 @@ fn build_da_blob_from_extrinsics(
                     .map(encrypted_note_bytes)
                     .collect::<Vec<_>>(),
             ),
+            ShieldedPoolCall::mint_coinbase { coinbase_data } => {
+                Some(vec![encrypted_note_bytes(&coinbase_data.encrypted_note)])
+            }
             ShieldedPoolCall::shielded_transfer_sidecar {
                 ciphertext_hashes,
                 ciphertext_sizes,
@@ -1464,6 +1467,9 @@ fn da_layout_from_extrinsics(
                     .map(|note| encrypted_note_bytes(note).len())
                     .collect::<Vec<_>>(),
             ),
+            ShieldedPoolCall::mint_coinbase { coinbase_data } => Some(vec![
+                encrypted_note_bytes(&coinbase_data.encrypted_note).len(),
+            ]),
             ShieldedPoolCall::shielded_transfer_sidecar {
                 ciphertext_sizes,
                 ..
